123647f5fd202427098358b766c7d383b0f6e27a
Bhuvaneshwar Kosaraju <108873725+BhuvaneshwarKosaraju@users.noreply.github.com>
2022-11-14 14:18:12 +0530

    element.sh

--- element.sh
+++ element.sh
@@ -1,32 +1,22 @@
-#! /bin/bash
+#!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
-PSQL="psql --username=freecodecamp --dbname=periodic_table -t --tuples-only -c"
-
-echo -e "\n\n~~~~ Periodic Table ~~~~\n\n"
-
-if [[ -z $1 ]]
-then
-  echo -e "\nPlease provide an element as an argument."
+if [[ -z $1 ]];then
+  echo "Please provide an element as an argument."
   exit
 fi
 
-#if argument is atomic number
-if [[ $1 =~ ^[1-9]+$ ]]
-then
-  element=$($PSQL "select atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where atomic_number = '$1'")
+if [[ $1 =~ ^[0-9]+$ ]];then
+  ELEMENT=$($PSQL "SELECT atomic_number,atomic_mass,symbol,name,type,melting_point_celsius,boiling_point_celsius FROM elements LEFT JOIN properties USING (atomic_number) LEFT JOIN types USING (type_id) WHERE atomic_number=$1")
 else
-#if argument is string
-  element=$($PSQL "select atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where name = '$1' or symbol = '$1'")
+  ELEMENT=$($PSQL "SELECT atomic_number,atomic_mass,symbol,name,type,melting_point_celsius,boiling_point_celsius FROM elements LEFT JOIN properties USING (atomic_number) LEFT JOIN types USING (type_id) WHERE symbol = '$1' OR name = '$1'")
 fi
 
-#element not in db
-if [[ -z $element ]]
-then
-  echo -e "\nI could not find that element in the database."
+if [[ -z $ELEMENT ]];then
+  echo "I could not find that element in the database."
   exit
 fi
 
-echo $element | while IFS=" |" read an name symbol type mass mp bp 
-do
-  echo -e "\nThe element with atomic number $an is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $mp celsius and a boiling point of $bp celsius."
+echo $ELEMENT | while IFS=" |" read A_NUMBER A_MASS SYMBOL NAME TYPE M_POINT B_POINT ; do
+  echo "The element with atomic number $A_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $A_MASS amu. $NAME has a melting point of $M_POINT celsius and a boiling point of $B_POINT celsius."
 done
